11919bff9daf4c063dc5c2b8c05104acccad0dd3
Abdelrhman <adodoadodo2025@gmail.com>
2026-06-22 22:09:03 +0300

    updated the database structure  , addded new columns

--- Database/DB Structure.sql
+++ Database/DB Structure.sql
@@ -6,6 +6,7 @@ CREATE TABLE users
   phone_number VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
   created_at TIMESTAMP NOT NULL,
+  address VARCHAR(200) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (email),
   UNIQUE (phone_number)
@@ -20,6 +21,8 @@ CREATE TABLE restaurants
   image_url VARCHAR(300) NOT NULL,
   is_busy BOOLEAN NOT NULL,
   rating NUMERIC(2, 1) NOT NULL,
+  delivery_time VARCHAR(100) NOT NULL,
+  delivery_fee INT NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -30,8 +33,10 @@ CREATE TABLE orders
   status VARCHAR(100) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   user_id INT NOT NULL,
+  restaurant_id INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT,
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants(id) ON DELETE RESTRICT
 );
 
 CREATE TABLE menu_items
@@ -41,6 +46,7 @@ CREATE TABLE menu_items
   description VARCHAR(500) NOT NULL,
   price FLOAT NOT NULL,
   cuisine_tag VARCHAR(100) NOT NULL,
+  image_url VARCHAR(500) NOT NULL,
   restaurant_id INT NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (restaurant_id) REFERENCES restaurants(id) ON DELETE CASCADE
